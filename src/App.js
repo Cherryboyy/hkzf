56e561e009c1d301c515ca8679a445ce1ff6981b
cmy <1732728869@qq.com>
2020-03-10 09:41:21 +0800

    完成两个路由配制

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 
-import {Button} from 'antd-mobile'
-
+import { Button } from 'antd-mobile'
+import Home from './pages/Home'
+import Citylist from './pages/Citylist'
+//配制路由
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 export default class App extends Component {
+
   render() {
     return (
-      <div>
-        我是根组件app
-        <Button type="primary">按钮</Button>
-      </div>
+      <Router>
+        <div>
+          {/* 我是根组件app
+        <Button type="primary">按钮</Button> */}
+          {/* 配制路由 */}
+          <Route path="/home" component={Home}></Route>
+          <Route path="/citylist" component={Citylist}></Route>
+        </div>
+      </Router>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+//导入全局样式文件
 import './index.css'
+//导入antd-mobile样式
 import 'antd-mobile/dist/antd-mobile.css'
+//导入app组件
 import App from './App'
 ReactDom.render(<App />, document.getElementById("root"))
